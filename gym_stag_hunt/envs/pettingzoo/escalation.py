5ff64020207e8aa8276a7876e2cc9e93bf273669
NullDefault <davisha999@gmail.com>
2022-03-26 18:02:22 -0400

    IT WORKS

--- gym_stag_hunt/envs/pettingzoo/escalation.py
+++ gym_stag_hunt/envs/pettingzoo/escalation.py
@@ -3,12 +3,8 @@ from gym_stag_hunt.envs.pettingzoo.shared import default_wrappers, PettingZooEnv
 from pettingzoo.utils import parallel_to_aec
 
 
-def make_env(raw=False, **kwargs):
-    return raw_env(**kwargs) if raw else env(**kwargs)
-
-
 def env(**kwargs):
-    return default_wrappers(ZooEscalationEnvironment(**kwargs))
+    return ZooEscalationEnvironment(**kwargs)
 
 
 def raw_env(**kwargs):
